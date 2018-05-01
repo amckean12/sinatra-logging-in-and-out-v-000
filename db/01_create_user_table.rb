608cb8bd2c09c59212ab2be103e0ae2741176aee
Alex McKean <amckean@me.com>
2018-05-01 17:37:12 +0000

    automatically backed up by learn

--- db/01_create_user_table.rb
+++ db/01_create_user_table.rb
@@ -1,9 +1,9 @@
 class CreateUsersTable < ActiveRecord::Migration
-  def change 
+  def change
     create_table :users do |t|
       t.string :user_name
       t.string :password
       t.decimal :balance
-  end 
-end
+    end
+  end
 end
