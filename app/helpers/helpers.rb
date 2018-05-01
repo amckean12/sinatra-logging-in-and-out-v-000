14e087c94ac3773ba4f89fd6847dc8eab8899193
Alex McKean <amckean@me.com>
2018-05-01 18:30:19 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -6,7 +6,7 @@ class Helpers
     @user = User.find_by_id(session_hash[:user_id])
   end
 
-  def self.is_logged_in?(session_hash)
+  def self.is_logged_in?(session)
     !!session_hash[:user_id]
   end
 
